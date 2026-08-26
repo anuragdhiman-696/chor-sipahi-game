8c31ad0ed171690d25b2724242af4d9fa53edee3
Anurag Dhiman <anuragdhiman696@gmail.com>
2026-08-26 16:38:52 +0530

    H

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Peer from 'peerjs';
 import './App.css';
 
 // Change URL to match your Express server port (default 5000 or process.env.PORT)
-const SOCKET_SERVER_URL = 'http://localhost:5000';
+const SOCKET_SERVER_URL = 'http://localhost:4000';
 
 const socket = io(SOCKET_SERVER_URL, { autoConnect: true });
 
